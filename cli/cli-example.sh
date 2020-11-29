a67f137a1b3077418bfb40a9befd0d8e4eef9ed8
WellRedPandit <WellRedPandit@users.noreply.github.com>
2020-11-29 00:35:47 -0500

    -

---
+++ cli/cli-example.sh
@@ -0,0 +1,1 @@
+./single-file https://medium.com/@blanchon.vincent blanchon.html --browser-headless=false --browser-executable-path=/Applications/Chromium.app/Contents/MacOS/Chromium
--- cli/single-file-cli-api.js
+++ cli/single-file-cli-api.js
@@ -203,7 +203,7 @@ async function capturePage(options) {
 	try {
 		const pageData = await backend.getPageData(options);
 		if (options.output) {
-			const filename = getFilename(pageData.output, options);
+			const filename = getFilename(options.output, options);
 			if (filename) {
 				fs.writeFileSync(options.output, pageData.content);
 			}
